43b3709f37ef07b5f567d40cceefcaca8d69a704
Mikron Licud <leinmik.ml@gmail.com>
2026-05-14 22:03:56 +0100

    #31 (#32)
    completed updating reports to use work items

--- source/MyKhronus.DataAccess/WorkItems/Models/WorkItemGetFilter.cs
+++ source/MyKhronus.DataAccess/WorkItems/Models/WorkItemGetFilter.cs
@@ -7,4 +7,6 @@ public record WorkItemGetFilter
     public Guid? WorkItemId { get; set; }
 
     public string Description { get; set; } = string.Empty;
+
+    public IEnumerable<Guid> WorkItemIds { get; set; } = Enumerable.Empty<Guid>();
 }
